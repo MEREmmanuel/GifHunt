1df2c3faa2b8dd58e3bb648023d5a542e5cb313f
MEREmmanuel <informatica_21887037@cintalapa.tecnm.mx>
2023-06-30 14:02:07 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import requests
+
+# Flask instance
+app = Flask(__name__)
+
+# First route
+
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method=='GET':
+        return render_template('index.html')
+    if request.form['search']:
+        url = "https://api.giphy.com/v1/gifs/search?api_key=ql66snaJwZsfCk7b1WmRhUcuIdwvEVBc&limit=20&q=" + request.form['search']
+        giphy = requests.get(url)
+        dataGiphy = giphy.json()
+        return render_template('index.html',data = dataGiphy['data'])
+    else:
+        return render_template('index.html')
+
+def pagina_no_encontrada(error):
+    return render_template('404.html'), 404
+
+app.register_error_handler(404, pagina_no_encontrada)
+
+if __name__ == '__main__':
+    app.run(debug=True)
